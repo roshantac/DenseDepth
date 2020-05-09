69fb9ac102af231ece35cf1e8c421e360606e49b
roshantac <muhammedroshan222@gmail.com>
2020-05-09 14:02:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from PIL import Image
 def DepthNorm(x, maxDepth):
     return maxDepth / x
 
-def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
+def predict(model, images, minDepth=10, maxDepth=1000, batch_size=32):
     # Support multiple RGBs, one RGB image, even grayscale 
     if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
     if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
